b84b6596d4bcb4c77f570616979a94e795642b03
Jose Ilitzky <josilit@gmail.com>
2025-12-08 15:25:25 -0800

    Finished AABB documentation

--- include/AABB.h
+++ include/AABB.h
@@ -4,56 +4,56 @@
 /// An Axis-Aligned Bounding Box (AABB) defined by its minimum and maximum points.
 struct AABB
 {
-    /// The minimum point of the bounding box (the bottom-left corner).
+    /// The bottom-left corner of the bounding box.
     Vector2 min;
-    /// The maximum point of the bounding box (the top-right corner).
+    /// The top-right corner of the bounding box.
     Vector2 max;
     
-    /// Constructor that initializes the AABB to the given minimum and maximum points.
-    /// @param min Bottom-left corner of the box.
-    /// @param max Top-right corner of the box.
+    /// Constructor that initializes the bounding box to the given minimum and maximum points.
+    /// @param min The minimum point (bottom-left corner).
+    /// @param max The maximum point (top-right corner).
     AABB(const Vector2& min, const Vector2& max) : min(min), max(max) {}
 
-    /// Calculates and returns the width of the bounding box.
-    /// @return The width of the bounding box.
+    /// Calculates the width of the bounding box.
+    /// @return The measured width.
     float GetWidth() const
     {
         return max.x - min.x;
     }
     
-    /// Calculates and returns the height of the bounding box.
-    /// @return The height of the bounding box.
+    /// Calculates the height of the bounding box.
+    /// @return The measured height.
     float GetHeight() const
     {
         return max.y - min.y;
     }
     
-    /// Calculates and returns the center point of the bounding box.
-    /// @return The vector representing the center point.
+    /// Calculates the center point of the bounding box.
+    /// @return The position representing the center point.
     Vector2 GetCenter() const
     {
         return (min + max) * 0.5f;
     }
     
-    /// Returns true if this AABB completely contains the other AABB.
-    /// @param other The other AABB to check for containment.
-    /// @return True if the other AABB is entirely within this box, false otherwise.
+    /// Returns true if this bounding box completely contains the other box.
+    /// @param other The other box to check.
+    /// @return True if the other box is entirely within this box, false otherwise.
     bool Contains(const AABB& other) const
     {
         return other.min.x >= min.x && other.max.x <= max.x && other.min.y >= min.y && other.max.y <= max.y;
     }
     
-    /// Returns true if this AABB contains the given position.
-    /// @param position The position to check for containment.
-    /// @return True if the position is inside or on the boundary of this AABB, false otherwise.
+    /// Returns true if this bounding box contains the given position.
+    /// @param position The position to check.
+    /// @return True if the position is inside or on the boundary of this box, false otherwise.
     bool Contains(const Vector2& position) const
     {
         return position.x >= min.x && position.y >= min.y && position.x <= max.x && position.y <= max.y;
     }
     
-    /// Returns true if this AABB overlaps the other AABB.
-    /// @param other The other AABB to check for intersection against.
-    /// @return True if the two AABBs overlap, false otherwise.
+    /// Returns true if this bounding box overlaps the other box.
+    /// @param other The other box to check against.
+    /// @return True if the two boxes overlap, false otherwise.
     bool Intersects(const AABB& other) const
     {
         if (max.x < other.min.x || min.x > other.max.x)
